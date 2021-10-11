72c0be5dc3fdf0c85b15791cf0fb55467aee246b
yechan <yyechan@naver.com>
2021-10-11 15:20:15 +0900

    2021-10-11 Initial commit

---
+++ src/main/java/hello/springmvcitemservice/SpringmvcItemserviceApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvcitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcItemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcItemserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvcitemservice/SpringmvcItemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcItemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
