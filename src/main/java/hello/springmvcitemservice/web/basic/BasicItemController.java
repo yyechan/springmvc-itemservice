1a59b2554fc37273fed866b9ca4113565c10eded
yechan <yyechan@naver.com>
2021-10-12 15:50:55 +0900

    2021-10-12
    item edit, redirect

--- src/main/java/hello/springmvcitemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvcitemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -55,13 +56,25 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item){
-        itemRepository.save(item);
-        //model.addAttribute("item", item); 생략가능
-        return "/basic/item";
+    public String addItemV2(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{itemId}";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId,Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
 
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Item item){
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     @PostConstruct
     public void init(){
